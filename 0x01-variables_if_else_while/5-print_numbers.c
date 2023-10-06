2c12444a6e9b9b13ca8bd3df40d86ad439410935
BlackOligarch <godfred.hammond2018@yahoo.com>
2023-10-06 19:38:19 +0000

    Added a C file that prints bas 10 digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main -The entry to the porgram
+ * Description: This program prints all single
+ * digit numbers of base 10
+ * Return: The return value is 0
+ */
+int main(void)
+{
+	int i = 0;
+	/* beginning of the while loop */
+	while (i <= 0)
+	putchar(i);
+	i++;
+	putchar('\n');
+	return (0);
+}
