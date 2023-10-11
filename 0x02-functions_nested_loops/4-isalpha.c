3bb1da9d9400be9496085c2ba39bd8b454ca744d
BlackOligarch <godfred.hammond2018@yahoo.com>
2023-10-11 08:48:28 -0700

    a function that checks for alphabetic character

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,9 +3,9 @@
 /**
  * _isalpha - checks for alphabetic characters.
  * @c: the character to be checked.
- * Rteurn: 1 if c is a letter 0r 0 otherwise.
+ * Return: 1 if c is a letter 0r 0 otherwise.
  */
 int _isalpha(int c)
 {
-	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
