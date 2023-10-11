af0646c4175a2948c7631efd06eceede7ab5849e
BlackOligarch <godfred.hammond2018@yahoo.com>
2023-10-11 06:49:05 -0700

    Added a C function and nested loops tasks

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds 2 integers & returns teh result.
+ * @a: int to be added to b.
+ * @b: int to be added to a.
+ *
+ * Return: sum of a & b
+ */
+int add(int a, int b)
+{
+  int c;
+  /* Initialize c */
+  c = a + b;
+
+  return (c);
+}
