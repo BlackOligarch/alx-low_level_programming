77d96e58f60b5116c8c3ffd2ebd7632dad6b0162
BlackOligarch <godfred.hammond2018@yahoo.com>
2023-10-20 19:43:02 +0000

    Added a C program 102-magic.c

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -2,9 +2,8 @@
 /**
  * main - the entry point of the program
  *
- * Return: the value is 0.
+ * Return: the return value is 0
  */
-
 int main(void)
 {
 int n;
@@ -13,6 +12,14 @@ int *p;
 
 a[2] = 1024;
 p = &n;
+/*
+* write your line of code here...
+* Remember:
+* - you are not allowed to use a
+* - you are not allowed to modify p
+* - only one statement
+* - you are not allowed to code anything else than this line of code
+*/
 *(p + 5) = 98;
 /* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
