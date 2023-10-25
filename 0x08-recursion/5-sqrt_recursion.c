c40249d2ab24764c1872ea2e8cd0fdd5900af867
BlackOligarch <godfred.hammond2018@yahoo.com>
2023-10-25 18:41:12 +0000

    Added a recursive function returns the natural square root of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * power_root - return the square root of a no.
+ *
+ * @n: value
+ * @c: count variable
+ *
+ * Return: the square root/ -1
+ */
+
+int power_root(int n, int c)
+{
+	if (c * c == n)
+	{
+		return (c);
+	}
+	else if (c * c > 4)
+	{
+		return (-1);
+	}
+	return (power_root(n, c + 1));
+}
+/**
+ * _sqrt_recursion - returns the square rootof a no.
+ *
+ * @n: value
+ *
+ * Return: the square root
+ */
+int _sqrt_recursion(int n)
+{
+	return (power_root(n, 0));
+}
