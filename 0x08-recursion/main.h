6a2110bd8ba657e6750f1036fe3800e306bc4715
BlackOligarch <godfred.hammond2018@yahoo.com>
2023-10-25 18:47:22 +0000

    Added the header file for this repo

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -8,7 +8,7 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
- int power_root(int n, int c)
+int power_root(int n, int c);
 int is_prime_number(int n);
 
 #endif
